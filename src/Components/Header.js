c47b32f613a7b55ba6ca4578dce715ff9b8d8f1f
elduke717 <spacematcha@proton.me>
2023-09-08 18:50:09 -0400

    style changes

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -1,13 +1,15 @@
-import React from 'react';
-
+import React from "react";
 
 const Header = () => {
-    return (
-        <header className="weather-header">
-            <h1>Weather App</h1>
-            <p>Your go-to source for current weather information!</p>
-        </header>
-    );
-}
+  return (
+    <header className="weather-header">
+      <div className="weatherData">
+        <img src="/public/weather_app_logo_clear.png" alt="Weather App Icon" />
+        <h1>Weather App</h1>
+      </div>
+      <p>Your go-to source for current weather information!</p>
+    </header>
+  );
+};
 
 export default Header;
